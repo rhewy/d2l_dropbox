3d04a7d0f40788a9f5fc05f9f22485e6e00c22e1
Robert Hewlett <rob.hewy@gmail.com>
2013-10-22 11:45:45 -0700

    More robust ... added a try catch

--- remove_d2litem_db/src/rob/run/Main.java
+++ remove_d2litem_db/src/rob/run/Main.java
@@ -19,42 +19,47 @@ package rob.run;
 //    --> Updated MMM-DD-YYYY (fl)
 //
 // =============================================================================
+import java.awt.HeadlessException;
 import java.io.File;
 import javax.swing.JFileChooser;
 import rob.com.utils.D2L;;
 
 public class Main {
 	public static void main(String[] args) {
-		File dropbox_dir = null;
-		// ===========================================================
-		// Check the command line for a dir otherwise pop a file chooser
-		// ===========================================================
-		if (args.length > 0) {
-			dropbox_dir = new File(args[0]);
-		} else {
-			JFileChooser getPath = new JFileChooser();
-			getPath.showDialog(null, "Select the dropbox Dir");
-			dropbox_dir = getPath.getSelectedFile();
-
-		}
-
-		File tmpFile, destFile;
-		// ===========================================================
-		// Strip the item and db ids off the file
-		// ===========================================================
-		String[] files = dropbox_dir.list();
-		String shortName;
-		for (String file : files) {
-			if (D2L.hasPatternItemDB(file)) {
-				tmpFile = new File(dropbox_dir, file);
-				shortName = D2L.getNoItemNoDBName(file);
-				destFile = new File(dropbox_dir, shortName);
-				tmpFile.renameTo(destFile);
-
-				System.out.printf("Renaming %s ----->  %s ...... %n", file,
-						shortName);
+		try {
+			File dropbox_dir = null;
+			// ===========================================================
+			// Check the command line for a dir otherwise pop a file chooser
+			// ===========================================================
+			if (args.length > 0) {
+				dropbox_dir = new File(args[0]);
+			} else {
+				JFileChooser getPath = new JFileChooser();
+				getPath.showDialog(null, "Select the dropbox Dir");
+				dropbox_dir = getPath.getSelectedFile();
 
 			}
+
+			File tmpFile, destFile;
+			// ===========================================================
+			// Strip the item and db ids off the file
+			// ===========================================================
+			String[] files = dropbox_dir.list();
+			String shortName;
+			for (String file : files) {
+				if (D2L.hasPatternItemDB(file)) {
+					tmpFile = new File(dropbox_dir, file);
+					shortName = D2L.getNoItemNoDBName(file);
+					destFile = new File(dropbox_dir, shortName);
+					tmpFile.renameTo(destFile);
+
+					System.out.printf("Renaming %s ----->  %s ...... %n", file,
+							shortName);
+
+				}
+			}
+		} catch (HeadlessException e) {
+			e.printStackTrace();
 		}
-	}
-}
+	} // end of the method main
+} // end of the class Main
